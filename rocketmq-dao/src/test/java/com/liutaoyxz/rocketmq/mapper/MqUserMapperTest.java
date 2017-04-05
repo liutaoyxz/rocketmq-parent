4fdfe57b58bffc95f80aab8af993e5b9e58f5aa4
liutaoyxz <18640076373@163.com>
2017-04-05 15:18:35 +0800

    增加多个模块

---
+++ rocketmq-dao/src/test/java/com/liutaoyxz/rocketmq/mapper/MqUserMapperTest.java
@@ -0,0 +1,34 @@
+package com.liutaoyxz.rocketmq.mapper;
+
+import com.liutaoyxz.rocketmq.entity.MqUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ltlxy on 2017/4/5.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:xml/spring-mybatis.xml")
+public class MqUserMapperTest {
+
+    @Autowired
+    private MqUserMapper mqUserMapper;
+
+    @Test
+    public void insert() throws Exception {
+
+        MqUser user = new MqUser();
+        user.setUserName("liutao");
+        user.setUserScore(100);
+        int i = this.mqUserMapper.insert(user);
+        System.out.println("insert num is:"+i);
+        System.out.println("userId is :"+user.getUserId());
+
+    }
+
+}
