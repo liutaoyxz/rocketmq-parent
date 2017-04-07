9cdba594bf4e464a0c6cd54180d95aafc743bda1
liutaoyxz <18640076373@163.com>
2017-04-07 14:00:59 +0800

    添加公司 和 家两个环境, 采用maven -P 过滤

--- rocketmq-user/src/test/java/com/liutaoyxz/rocketmq/user/api/IUserApiImplTest.java
+++ rocketmq-user/src/test/java/com/liutaoyxz/rocketmq/user/api/IUserApiImplTest.java
@@ -19,7 +19,6 @@ public class IUserApiImplTest {
 
     @Test
     public void queryUserById() throws Exception {
-
         System.out.println(iUserApi.queryUserById(1));
 //        Thread.sleep(10000000L);
     }
